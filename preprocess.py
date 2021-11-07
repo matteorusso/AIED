471be2d88ea672d49b5e1b06d99e313ddfdba03d
Matteo Russo <matteor@princeton.edu>
2021-11-07 22:26:24 +0100

    Prelim stats

--- preprocess.py
+++ preprocess.py
@@ -15,25 +15,33 @@ birth_year = user_profile['birth'].to_dict()
 def age_convert(y):
     if y == None or math.isnan(y):
         return 0
-    a = 2018 - int(y)
+    a = 2018 - int(y) # leave it as 2018 because people were taking courses back then
     if a> 70 or a< 10:
         a = 0
     return a
 all_feat['age'] = [age_convert(birth_year.get(int(u),None)) for u in all_feat['username']]
 
 # extract user gender
-user_gender = user_profile['gender'].to_dict()
+user_gender = dict(zip(user_profile.index, user_profile.gender))#user_gender = user_profile['gender'].to_dict()
 def gender_convert(g):
-    if g == 'm':
+    if g == 'male':
         return 1
-    elif g == 'f':
+    elif g == 'female':
         return 2
     else:
         return 0
-
-
 all_feat['gender'] = [gender_convert(user_gender.get(int(u),None)) for u in all_feat['username']]
+# user_gender = user_profile['gender'].to_dict()
+# def gender_convert(g):
+#     if g == 'm':
+#         return 1
+#     elif g == 'f':
+#         return 2
+#     else:
+#         return 0
+# all_feat['gender'] = [gender_convert(user_gender.get(int(u),None)) for u in all_feat['username']]
 
+# extract user education
 user_edu = user_profile['education'].to_dict()
 def edu_convert(x):
     edus = ["Bachelor's","High", "Master's", "Primary", "Middle","Associate","Doctorate"]
@@ -57,23 +65,23 @@ all_feat = pd.merge(all_feat, course_enroll_num, left_on='course_id', right_inde
 
 
 #extract user cluster
-user_cluster_id = pkl.load(open('cluster/user_dict','r'))
+user_cluster_id = pkl.load(open('cluster/user_dict','rb'), encoding='latin1')
 cluster_label = np.load('cluster/label_5_10time.npy')
 all_feat['cluster_label'] = [cluster_label[user_cluster_id[u]] for u in all_feat['username']]
 
 
 #extract course category
 courseinfo = pd.read_csv('course_info.csv', index_col='id')
-en_categorys = ['math','physics','electrical', 'computer','foreign language', 'business', 'economics','biology','medicine','literature','philosophy','history','social science', 'art','engineering','education','environment','chemistry']
+en_categories = ['math','physics','electrical', 'computer','foreign language', 'business', 'economics','biology','medicine','literature','philosophy','history','social science', 'art','engineering','education','environment','chemistry']
 
 def category_convert(cc):
     if isinstance(cc, str):
-        for i, c in zip(range(len(en_categorys)), en_categorys):
+        for i, c in zip(range(len(en_categories)), en_categories):
             if cc == c:
                 return i+1
     else:
         return 0
-category_dict = courseinfo['category'].to_dict()
+category_dict = dict(zip(courseinfo.course_id, courseinfo.category)) #category_dict = courseinfo['category'].to_dict()
 
 all_feat['course_category'] = [category_convert(category_dict.get(str(x), None)) for x in all_feat['course_id']]
 
@@ -82,6 +90,12 @@ act_feats = [c for c in train_feat.columns if 'count' in c or 'time' in c or 'nu
 pkl.dump(act_feats, open('act_feats.pkl','wb'))
 
 num_feats = act_feats + ['age','course_enroll_num','user_enroll_num']
+
+# Unscaled data
+all_feat.loc[train_feat.index].to_csv('u_train_feat.csv')
+all_feat.loc[test_feat.index].to_csv('u_test_feat.csv')
+
+# Scaled data
 scaler= StandardScaler()
 newX = scaler.fit_transform(all_feat[num_feats])
 print(newX.shape)
